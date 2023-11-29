3a7afcb7482fad198691cdb9c487f3eea3d28b7a
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2023-11-29 18:55:15 +0900

    add merged file

---
+++ airflow_exercise/airflow_merge/DAG/dag.py
@@ -0,0 +1,83 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook # 추가
+
+import sys, os
+sys.path.append(os.getcwd())
+
+from crawling.crawling_event import *
+from crawling.crawling_velog import *
+
+def upload_to_s3() :
+    date = datetime.now().strftime("%Y%m%d")
+    hook = S3Hook('de_velog_aws') # connection ID 입력
+    
+    event_filename = f'/home/ubuntu/airflow/airflow/data/event_{date}.csv'
+    velog_filename = f'/home/ubuntu/airflow/airflow/data/velog_{date}.csv'
+
+    event_key = f'data/event_{date}.csv'
+    velog_key = f'data/velog_{date}.csv'
+
+    bucket_name = 'khuda-de-project' 
+    hook.load_file(filename=event_filename, key=event_key, bucket_name=bucket_name)
+    hook.load_file(filename=velog_filename, key=velog_key, bucket_name=bucket_name)
+
+default_args = {
+    'owner': 'owner-name',
+    'depends_on_past': False,
+    'email': ['your-email@g.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=30),
+}
+
+dag_args = dict(
+    dag_id="crawling-upload",
+    default_args=default_args,
+    description='KHUDA de-project DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 11, 29),
+    tags=['de-project'],
+)
+
+with DAG( **dag_args ) as dag:
+    start = BashOperator(
+        task_id='start',
+        bash_command='echo "start"',
+    )
+
+    upload = PythonOperator(
+        task_id = 'upload',
+        python_callable = upload_to_s3
+    )
+    
+    # -------- velog -------- #
+    velog_get_url_task = PythonOperator(
+        task_id='velog_get_url',
+        python_callable= velog_get_url
+    )
+
+    velog_get_info_task= PythonOperator(
+        task_id='velog_get_info',
+        python_callable= velog_get_info
+    )
+
+    # -------- event -------- #
+    event_get_data_task = PythonOperator(
+        task_id="event_get_data", 
+        python_callable= event_get_data
+    )
+
+    # -------- contest -------- #
+
+    complete = BashOperator(
+        task_id='complete_bash',
+        bash_command='echo "complete"',
+    )
+
+    start >> event_get_data_task >> upload >> complete
+    start >> velog_get_url_task >> velog_get_info_task >> upload >> complete
