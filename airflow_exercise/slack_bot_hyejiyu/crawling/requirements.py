d99b7187a0c9d5b4721295bc55378513c404acd5
HyejiYu <90139122+HyejiYu@users.noreply.github.com>
2023-12-02 22:28:01 +0900

    Add files via upload

---
+++ airflow_exercise/slack_bot_hyejiyu/crawling/requirements.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime 
+import time,os,requests, re
+import requests.exceptions as requests_exceptions
+import pandas as pd
+import numpy as np
+import json, pathlib
+import warnings
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup as bs
+import asyncio
+import aiohttp
+import csv
