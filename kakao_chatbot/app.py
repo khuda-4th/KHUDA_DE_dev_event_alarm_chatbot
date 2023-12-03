cd2cd7f32b8d7d5e12bdf67c40c6cc0f1b2406af
Sangjin Han <49024115+eu2525@users.noreply.github.com>
2023-12-03 18:15:42 +0900

    Update app.py

--- kakao_chatbot/app.py
+++ kakao_chatbot/app.py
@@ -8,8 +8,7 @@ def hello():
     return 'Hello World!'
 
 @app.route("/dev_event", methods= ["POST"])
-def main_page():
-        # 검색 타입(예매 순위 or 개봉 예정작)과 검색 시작 번호를 movie_search 함수로 전달하여 아이템과 버튼 설정을 반환받음  
+def dev_event():
     listItems = make_devcard()
     # 카드 리스트형 응답용 메시지
     res = {
@@ -50,8 +49,7 @@ def velog_event():
     return jsonify(res)
 
 @app.route("/contest_event", methods= ["POST"])
-def contest_event():
-        # 검색 타입(예매 순위 or 개봉 예정작)과 검색 시작 번호를 movie_search 함수로 전달하여 아이템과 버튼 설정을 반환받음  
+def contest_event():  
     listItems = make_eventcard()
     # 카드 리스트형 응답용 메시지
     res = {
@@ -70,8 +68,6 @@ def contest_event():
 
     # 전송
     return jsonify(res)
-
-
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
